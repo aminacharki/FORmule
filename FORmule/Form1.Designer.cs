01258457bb406cd05914572ba52b1212827b207c
amina charki <amina charki@AMINA>
2022-12-02 00:42:04 +0100

    C

--- FORmule/Form1.Designer.cs
+++ FORmule/Form1.Designer.cs
@@ -63,30 +63,30 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(198, 43);
+            this.textBox1.Location = new System.Drawing.Point(199, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(198, 80);
+            this.textBox2.Location = new System.Drawing.Point(199, 80);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(198, 122);
+            this.textBox3.Location = new System.Drawing.Point(199, 122);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(164, 20);
             this.textBox3.TabIndex = 4;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(198, 162);
+            this.textBox4.Location = new System.Drawing.Point(199, 162);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(164, 20);
             this.textBox4.TabIndex = 5;
             // 
             // color
@@ -109,54 +109,74 @@
             // 
             // buttajouute
             // 
-            this.buttajouute.Location = new System.Drawing.Point(90, 286);
+            this.buttajouute.BackColor = System.Drawing.Color.Gray;
+            this.buttajouute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttajouute.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttajouute.Location = new System.Drawing.Point(71, 286);
             this.buttajouute.Name = "buttajouute";
-            this.buttajouute.Size = new System.Drawing.Size(75, 23);
+            this.buttajouute.Size = new System.Drawing.Size(95, 46);
             this.buttajouute.TabIndex = 8;
-            this.buttajouute.Text = "ajouter";
-            this.buttajouute.UseVisualStyleBackColor = true;
+            this.buttajouute.Text = "Ajouter";
+            this.buttajouute.UseVisualStyleBackColor = false;
+            this.buttajouute.Click += new System.EventHandler(this.buttajouute_Click);
             // 
             // buttmodifier
             // 
-            this.buttmodifier.Location = new System.Drawing.Point(207, 286);
+            this.buttmodifier.BackColor = System.Drawing.Color.Gray;
+            this.buttmodifier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttmodifier.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttmodifier.Location = new System.Drawing.Point(199, 285);
             this.buttmodifier.Name = "buttmodifier";
-            this.buttmodifier.Size = new System.Drawing.Size(75, 23);
+            this.buttmodifier.Size = new System.Drawing.Size(91, 46);
             this.buttmodifier.TabIndex = 9;
-            this.buttmodifier.Text = "modifier";
-            this.buttmodifier.UseVisualStyleBackColor = true;
+            this.buttmodifier.Text = "Modifier";
+            this.buttmodifier.UseVisualStyleBackColor = false;
+            this.buttmodifier.Click += new System.EventHandler(this.buttmodifier_Click);
             // 
             // buttonsupp
             // 
+            this.buttonsupp.BackColor = System.Drawing.Color.Gray;
+            this.buttonsupp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonsupp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.buttonsupp.Location = new System.Drawing.Point(321, 286);
             this.buttonsupp.Name = "buttonsupp";
-            this.buttonsupp.Size = new System.Drawing.Size(75, 23);
+            this.buttonsupp.Size = new System.Drawing.Size(93, 46);
             this.buttonsupp.TabIndex = 10;
             this.buttonsupp.Text = "supprimer";
-            this.buttonsupp.UseVisualStyleBackColor = true;
+            this.buttonsupp.UseVisualStyleBackColor = false;
             // 
             // buttoncomfirm
             // 
-            this.buttoncomfirm.Location = new System.Drawing.Point(459, 286);
+            this.buttoncomfirm.BackColor = System.Drawing.Color.Gray;
+            this.buttoncomfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttoncomfirm.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttoncomfirm.Location = new System.Drawing.Point(448, 286);
             this.buttoncomfirm.Name = "buttoncomfirm";
-            this.buttoncomfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttoncomfirm.Size = new System.Drawing.Size(86, 46);
             this.buttoncomfirm.TabIndex = 11;
             this.buttoncomfirm.Text = "confirmer";
-            this.buttoncomfirm.UseVisualStyleBackColor = true;
+            this.buttoncomfirm.UseVisualStyleBackColor = false;
+            this.buttoncomfirm.Click += new System.EventHandler(this.buttoncomfirm_Click);
             // 
             // buttonannul
             // 
-            this.buttonannul.Location = new System.Drawing.Point(589, 286);
+            this.buttonannul.BackColor = System.Drawing.Color.Gray;
+            this.buttonannul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonannul.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonannul.Location = new System.Drawing.Point(579, 286);
             this.buttonannul.Name = "buttonannul";
-            this.buttonannul.Size = new System.Drawing.Size(75, 23);
+            this.buttonannul.Size = new System.Drawing.Size(85, 46);
             this.buttonannul.TabIndex = 12;
             this.buttonannul.Text = "annuler";
-            this.buttonannul.UseVisualStyleBackColor = true;
+            this.buttonannul.UseVisualStyleBackColor = false;
+            this.buttonannul.Click += new System.EventHandler(this.buttonannul_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(764, 450);
             this.Controls.Add(this.buttonannul);
             this.Controls.Add(this.buttoncomfirm);
             this.Controls.Add(this.buttonsupp);
--- FORmule/Form1.cs
+++ FORmule/Form1.cs
@@ -1,23 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace FORmule
 {
     public partial class Form1 : Form
     {
+        MySqlConnection MySQLConnection = new MySqlConnection();
+        MySqlDataAdapter MySQLAdapter = new MySqlDataAdapter();
+        String MySQLChaineConnection = "server=localhost; user id=root;password= ;database=carcopos"
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttajouute_Click(object sender, EventArgs e)
+        {
+            buttajouute.Enabled=false;
+            buttmodifier.Enabled=false;
+            buttoncomfirm.Enabled=true;
+        }
+
+        private void buttmodifier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttoncomfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonannul_Click(object sender, EventArgs e)
         {
 
         }
